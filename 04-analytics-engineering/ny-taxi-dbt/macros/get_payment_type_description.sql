02ebe19f4f816f70ecb4b8e897fa1b4fd4db5169
jun01ee <56535319+jun01ee@users.noreply.github.com>
2025-02-24 12:55:21 +0000

    use Wing's data

--- 04-analytics-engineering/ny-taxi-dbt/macros/get_payment_type_description.sql
+++ 04-analytics-engineering/ny-taxi-dbt/macros/get_payment_type_description.sql
@@ -1,15 +1,17 @@
-models:
-  taxi_rides_ny:
-      # Applies to all files under models/.../
-      staging:
-          materialized: view
-      core:
-          materialized: table
-vars:
-  payment_type_values: [1, 2, 3, 4, 5, 6]
+{#
+    This macro returns the description of the payment_type 
+#}
 
-seeds: 
-    taxi_rides_ny:
-        taxi_zone_lookup:
-            +column_types:
-                locationid: numeric
+{% macro get_payment_type_description(payment_type) -%}
+
+    case {{ dbt.safe_cast("payment_type", api.Column.translate_type("integer")) }}  
+        when 1 then 'Credit card'
+        when 2 then 'Cash'
+        when 3 then 'No charge'
+        when 4 then 'Dispute'
+        when 5 then 'Unknown'
+        when 6 then 'Voided trip'
+        else 'EMPTY'
+    end
+
+{%- endmacro %}
