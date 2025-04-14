7b5b247192d7390f08d6547d2621544abde37592
jun01ee <56535319+jun01ee@users.noreply.github.com>
2025-04-14 18:45:08 +0800

    updates

--- 07-project/scripts/ETL.py
+++
@@ -1,9 +0,0 @@
-import os
-import argparse
-
-from pyspark.sql import SparkSession
-from pyspark.sql import functions as F
-
-
-from google.cloud import bigquery
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/23491378/keys/dataengineering-project-456707-dd73f064e848.json'
